15e33eefa1938ee80b18c8464b5733c7a5fd15e9
luo <yes_wecode@163.com>
2024-10-03 13:14:55 +0800

    chore: update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,8 +12,10 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     // 关闭驼峰命名规则 使用之后eslint无法正常检查其他错误
     // 'vue/multi-word-component-names': 0
+    // 关闭对单个单词组件名称的限制
+    'vue/multi-word-component-names': 'off'
   }
 }
